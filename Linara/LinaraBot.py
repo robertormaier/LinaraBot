12469361ba6cb638f9395fd58fe0af9447dd955d
Roberto Maier <robertomaier02@gmail.com>
2017-10-09 19:32:15 -0300

    Add connecation with the bench of datas.

--- Linara/LinaraBot.py
+++ Linara/LinaraBot.py
@@ -3,28 +3,21 @@
 from twx.botapi import TelegramBot
 from time import sleep
 import pickle
-import mysqllb
+import mysql
 from os import path
 
 # Configuração para inciar o bot
 CURR_PATH = path.dirname(path.realpath(__file__))
-DUMP_FILE = path.join(CURR_PATH,"data.pkl")
+DUMP_FILE = path.join(CURR_PATH, "data.pkl")
 
-
-print ('Start connection of benck  of data ')
-cnx = mysql.connector.connect(user='scott', password='tiger',
-                              host='127.0.0.1',
-                              database='employees')
-cnx.close()
-
-BOTAPI = ''
+BOTAPI='401234240:AAGwJPXWF4Iz-g0PQ_JImXQoVbd0Sk6lS8g'
 bot = TelegramBot(BOTAPI)
 bot.update_bot_info().wait()
 
-print  (bot.username)
+print(bot.username)
 
 if path.exists(DUMP_FILE):  # Se existe, carregar a lista de mensagens respondidas
-    pkl_file = open(DUMP_FILE)
+    pkl_file = open(DUMP_FILE,'r')
     answered_messages = pickle.load(pkl_file)
 else:
     answered_messages = []
@@ -32,7 +25,7 @@ else:
 _bot_message = {'Oi, eu sou um bot, meu nome é Linara, e o seu? Em breve terei mais funcionalidades!'}
 
 while (True):
-    print ("Getting updates".center(50, '-'))
+    print("Getting updates".center(50, '-'))
 
     updates = bot.get_updates().wait()
     for pos, update in enumerate(updates):
