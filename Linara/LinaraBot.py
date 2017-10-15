f7db171dc4789d3fa1d0fcccac549cf67e2026b3
Roberto Maier <robertomaier02@gmail.com>
2017-10-15 02:33:59 -0200

    Introduction of aiml

--- Linara/LinaraBot.py
+++ Linara/LinaraBot.py
@@ -6,11 +6,6 @@ from os import path
 import os
 import aiml
 import traceback
-###############################################################################################################3
-
-# Configuração para inciar o bot
-CURR_PATH = path.dirname(path.realpath(__file__))
-DUMP_FILE = path.join(CURR_PATH, "data.pkl")
 
 token = '401234240:AAGwJPXWF4Iz-g0PQ_JImXQoVbd0Sk6lS8g'
 bot = TelegramBot(token)
